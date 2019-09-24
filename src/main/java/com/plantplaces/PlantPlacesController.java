7971aba947a7bf647b7494fbb6aba453536ae743
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-24 14:03:05 -0400

    Updating the .gitignore and the html page

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,13 +1,21 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantPlacesController {
 	
-	@RequestMapping("/start")
-	public String start()
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read()
+	{
+		return "start";
+	}
+	
+	@PostMapping(value="/start")
+	public String create()
 	{
 		return "start";
 	}
